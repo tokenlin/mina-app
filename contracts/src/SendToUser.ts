1d6f34096c1c688b6a0af78de9ad819015179e72
tokenlin <lkw040535@gmail.com>
2025-02-05 22:59:14 +0800

    first

---
+++ contracts/src/SendToUser.ts
@@ -0,0 +1,40 @@
+import {
+  Field,
+  state,
+  State,
+  method,
+  UInt64,
+  PrivateKey,
+  SmartContract,
+  Mina,
+  AccountUpdate,
+  Bool,
+  PublicKey,
+  DeployArgs,
+  Permissions,
+  UInt32
+} from 'o1js';
+
+export class SendToUser extends SmartContract {
+  async deploy(props?: DeployArgs) {
+    await super.deploy(props)
+    
+    // 初始化账户权限
+    this.account.permissions.set({
+      ...Permissions.default(),
+      setVerificationKey: Permissions.VerificationKey.impossibleDuringCurrentVersion(),
+      setPermissions: Permissions.impossible(),
+    })
+  }
+
+
+  @method
+  async sendToUser(sender: PublicKey, receiver: PublicKey, amount: UInt64) {
+
+    let senderUpdate = AccountUpdate.createSigned(sender);
+    senderUpdate.send({ to: receiver, amount: amount });
+
+  }
+
+
+}
